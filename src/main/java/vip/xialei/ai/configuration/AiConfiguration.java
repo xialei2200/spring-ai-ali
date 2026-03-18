d8e128b65df11f02a5c6967992be3943fba883b4
xialei2200 <xialei2200@126.com>
2026-03-18 22:27:04 +0800

    dashscope

--- src/main/java/vip/xialei/ai/configuration/AiConfiguration.java
+++ src/main/java/vip/xialei/ai/configuration/AiConfiguration.java
@@ -1,6 +1,11 @@
 package vip.xialei.ai.configuration;
 
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import com.alibaba.cloud.ai.dashscope.chat.observation.DashScopeChatModelObservationConvention;
 import io.micrometer.observation.ObservationRegistry;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.model.tool.ToolCallingManager;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -10,6 +15,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
 
 /**
  * Spring AI 配置类
@@ -21,6 +27,8 @@ public class AiConfiguration {
     private String closeAiApiKey;
     @Value("${spring.ai.close.ai.openai-base-url}")
     private String closeAiBaseUrl;
+    @Value("${spring.ai.close.ai.dashScopeApiKey}")
+    private String dashScopeApiKey;
 
     @Bean
     public OpenAiApi openAiApi() {
@@ -40,10 +48,35 @@ public class AiConfiguration {
     }
 
     @Bean("deepseek-v3.2")
-    public OpenAiChatModel chatModel(OpenAiApi openAiApi, OpenAiChatOptions openAiChatOptions) {
+    public OpenAiChatModel deepseek(OpenAiApi openAiApi, OpenAiChatOptions openAiChatOptions) {
         return new OpenAiChatModel(openAiApi, openAiChatOptions,
                 ToolCallingManager.builder().build(),
                 RetryUtils.DEFAULT_RETRY_TEMPLATE,
                 ObservationRegistry.NOOP);
     }
+    @Bean("qwen")
+    public ChatModel qwen() {
+        DashScopeApi dashScopeApi = DashScopeApi.builder()
+                .apiKey(dashScopeApiKey)
+                .baseUrl("https://dashscope.aliyuncs.com")
+                .build();
+        // Create DashScope ChatModel instance
+        DashScopeChatModel chatModel = DashScopeChatModel.builder()
+                .dashScopeApi(dashScopeApi)
+                .defaultOptions(DashScopeChatOptions.builder()
+                        .model("qwen3-max")
+                        .temperature(0.5)
+                        .maxToken(10000)
+                        .build())
+                .build();
+        Flux<String> stream = chatModel.stream("请写一个关于春天的现代诗，要求10行以内");
+        // 流式打印到控制台
+        System.out.println("流式打印：");
+        stream.subscribe(System.out::print);
+        System.out.println();
+        return chatModel;
+    }
+
+
+
 }
